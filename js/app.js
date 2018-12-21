f273361007a8e8a1ee4c0919fa10982b9c9ad56c
Taylor Swyter <tswyter@hubspot.com>
2018-12-21 11:38:41 -0500

    updates font-end styles to re-configure layout

--- js/app.js
+++ js/app.js
@@ -10,13 +10,15 @@ const feedr = {
     feedr.getNews('');
   },
   defineElements: () => {
-    feedr.resultsContainer = document.getElementById('main');
-    feedr.sourceContainer = document.querySelector('.sources');
-    feedr.modalContainer = document.querySelector('#popUp');
-    feedr.searchInput = document.querySelector('#search');
+    feedr.resultsContainer = document.querySelector('.feedr-content__articles');
+    feedr.sourceContainer = document.querySelector('.feedr-sources');
+    feedr.modalContainer = document.querySelector('.feedr-modal');
+    feedr.modalContent = feedr.modalContainer.querySelector('.feedr-modal__content');
+    feedr.modalTitle = feedr.modalContainer.querySelector('.feedr-modal__title');
+    feedr.searchInput = document.querySelector('.feedr-search__input');
   },
   addListeners: () => {
-    const closeModalButton = feedr.modalContainer.querySelector('.closePopUp');
+    const closeModalButton = feedr.modalContainer.querySelector('.feedr-modal__close');
     closeModalButton.addEventListener('click', feedr.toggleModal);
     feedr.searchInput.addEventListener('change', e => {
       feedr.searchTerm = e.currentTarget.value;
@@ -47,7 +49,7 @@ const feedr = {
   },
   updateResults: (results) => {
     feedr.resultsContainer.innerHTML = results.map(result => result.formatted).join('');
-    const articleCards = feedr.resultsContainer.querySelectorAll('.article');
+    const articleCards = feedr.resultsContainer.querySelectorAll('.feedr-article');
     articleCards.forEach(card => card.addEventListener('click', e => {
       const title = e.currentTarget.querySelector('h3').innerText;
       feedr.toggleModal(results.find(result => result.data.title === title));
@@ -55,51 +57,55 @@ const feedr = {
   },
   updateSources: (sources) => {
     feedr.sourceContainer.innerHTML = feedr.formatSourceFilters(sources).join('');
-    const sourceButtons = feedr.sourceContainer.querySelectorAll('li');
-    const selectedSourceDisplay = document.querySelector('.selected-source');
+    const sourceButtons = feedr.sourceContainer.querySelectorAll('.feedr-sources__button');
+    const selectedSourceDisplay = document.querySelector('.feedr-sources__source.selected');
     sourceButtons.forEach(button => {
       button.addEventListener('click', e => {
-        feedr.clickedSource = e.currentTarget.innerText;
-        selectedSourceDisplay.innerText = feedr.clickedSource;
+        sourceButtons.forEach(button => button.parentNode.classList.remove('selected'));
+        feedr.clickedSource = e.currentTarget;
+        feedr.clickedSource.parentNode.classList.add('selected');
       });
     });
   },
   toggleModal: (content) => {
-    const modalContent = feedr.modalContainer.querySelector('.container');
-    modalContent.innerHTML = '';
-    feedr.modalContainer.classList.toggle('hidden');
+    feedr.modalContent.innerHTML = '';
+    feedr.modalContainer.classList.toggle('show');
     feedr.modalContainer.classList.toggle('loader');
-    modalContent.innerHTML = feedr.formatModal(content);
+    feedr.modalContent.innerHTML = feedr.formatModal(content);
+    feedr.modalTitle.innerText = content.data.title;
   },
   formatModal: (article) => {
     const { title, content, source, url } = article.data;
     return `
-      <h1>${title}</h1>
-      <p>
-        ${content}
-      </p>
-      <a href="${url}" class="popUpAction" target="_blank">Read more from ${source.name}</a>
+      <div class="feedr-modal__article-text">
+        <p>${content}</p>
+      </div>
+      <div class="feedr-modal__source">
+        <a href="${url}">Read more from <span class="source">${source.name}</span></a>
+      </div>
     `;
   },
   formatFeedPost: (article) => {
     return `
-    <article class="article">
-      <section class="featuredImage">
-        <img src="${article.urlToImage}" alt="" />
-      </section>
-      <section class="articleContent">
-        <h3>${article.title}</h3>
-        <p>${article.description}</p>
-        <h6>${article.source.name}</h6>
-      </section>
-      <section class="impressions">
-        526
-      </section>
-      <div class="clearfix"></div>
-    </article>
+      <article class="feedr-article">
+        <div class="feedr-article__image-container">
+        ${article.urlToImage ? `
+          <a href="#">
+            <img class="feedr-article__image" src="${article.urlToImage}" alt="#" />
+          </a>
+        ` : ``}
+        </div>
+        <a class="feedr-article__text-container" href="#">
+          <h3 class="feedr-article__title">${article.title}</h3>
+          <p class="feedr-article__description">${article.description}</p>
+          <div class="feedr-article__source">
+            <h4 class="feedr-article__source-name">${article.source.name}</h4>
+          </div>
+        </a>
+      </article>
     `;
   },
-  formatSourceFilters: (sources) => sources.map(source => `<li>${source}</li>`),
+  formatSourceFilters: (sources) => sources.map(source => `<li class="feedr-sources__source"><button class="feedr-sources__button">${source}</button></li>`),
 
 };
 
